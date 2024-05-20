595f9612c4803b718957a17b6426d167ca5916d6
AKASHLM010 <Akashheaven2003@gmail.com>
2024-05-21 02:58:01 +0530

    ADDED USER AND VIDEO MODEL &  JWT AND BCRYPT  DONE

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,6 +4,12 @@ const asyncHandler = (requestHandler) => {
     }
 }
 
+// Why Return req, res, and next
+// By returning a function with the signature (req, res, next), asyncHandler ensures that the resulting function can be used as a standard Express route handler. This returned function does the following:
+
+// 1. Accepts the Same Parameters: It takes req, res, and next, just like any other Express middleware or route handler.
+// 2. Executes the Original Handler: It calls the requestHandler with req, res, and next.
+// 3. Handles Errors Automatically: It catches any errors from the requestHandler and passes them to next, which triggers the error-handling middleware.
 
 export { asyncHandler }
 
@@ -25,3 +31,27 @@ export { asyncHandler }
 //         })
 //     }
 // }
+
+// EXAMPLE:
+
+// Without asyncHandler:
+
+// app.get('/route', async (req, res, next) => {
+//     try {
+//         // Asynchronous code that might throw an error
+//         const data = await someAsyncFunction();
+//         res.send(data);
+//     } catch (err) {
+//         // Pass the error to Express error handler
+//         next(err);
+//     }
+// });
+// With asyncHandler:
+
+
+// app.get('/route', asyncHandler(async (req, res, next) => {
+//     // Asynchronous code
+//     const data = await someAsyncFunction();
+//     res.send(data);
+// }));
+// Using asyncHandler reduces boilerplate code and ensures consistent error handling across all routes.
